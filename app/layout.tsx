9cc6136634b67312e275a6089abe3d2426066fd5
Maschhindra Kotkar <machindra02kotkar@gmail.com>
2026-08-12 14:18:46 +0530

    Add Microsoft Clarity tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,16 @@ return (
       `}
     </Script>
 
+<Script id="microsoft-clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "y15195knb8");
+  `}
+</Script>
+
     <body suppressHydrationWarning>
       {children}
     </body>
